d8389392b91542731131134b196b7049ecbb2728
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 <learn.aayush@gmail.com>
2026-05-21 22:56:54 +0545

    try cicd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
                     to="/"
                     className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
-                    Register
+                    Register here
                   </Link>
                   <Link
                     to="/list"
